c15f541fbd881ed8f5fd019af5be97ba22bd5ce5
Pedro <pedrohlucena413@gmail.com>
2024-07-24 10:39:35 -0300

    feat(frontend): add file system structure

---
+++ packages/frontend/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import { StyledComponentsRegistry } from '@/lib'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Desafio Bondy',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
+    </html>
+  )
+}
